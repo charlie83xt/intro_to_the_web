ca3b347e232e50643ad103c7d5136318257a6390
AJOsmaston <dajoxx@gmail.com>
2021-10-04 15:58:16 +0100

    added /cat

--- app.rb
+++ app.rb
@@ -1,5 +1,16 @@
 require 'sinatra'
+require "sinatra/reloader" if development?
 
 get '/' do
   "Hello World"
+end
+
+get '/secret' do
+  "Hello yet again"
+end
+
+get '/cat' do
+  "<div style ='border: 3px dashed red'>
+    <img src='https://i.imgur.com/jFaSxym.png'>
+  </div>"
 end
