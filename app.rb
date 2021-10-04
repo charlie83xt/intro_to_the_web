c484978fe1bf643b6166b4d534db825a946fbb79
AJOsmaston <dajoxx@gmail.com>
2021-10-04 16:07:03 +0100

    cleaned up earlier exercises

--- app.rb
+++ app.rb
@@ -1,16 +1,6 @@
 require 'sinatra'
 require "sinatra/reloader" if development?
 
-get '/' do
-  "Hello World"
-end
-
-get '/secret' do
-  "Hello yet again"
-end
-
 get '/cat' do
-  "<div style ='border: 3px dashed red'>
-    <img src='https://i.imgur.com/jFaSxym.png'>
-  </div>"
+  erb(:index)
 end
